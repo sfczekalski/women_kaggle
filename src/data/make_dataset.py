896310bba5d5da4a0e0b19e21f37e07a8db68afd
sfczekalski <sfczekalski@gmail.com>
2020-05-17 18:43:16 +0200

    Put some preprocessing code to script.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,16 +4,20 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+# preprocessing
+from preprocess import preprocess_data, preprocess_skills_data
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+data_dir = '../data/'
+
+
+def main():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    preprocess_data(data_dir)
+    preprocess_skills_data(data_dir)
 
 
 if __name__ == '__main__':
